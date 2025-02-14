d425ea5e2a7d619ac421d6c15aa5b74009e96358
Swift <hellosai109@gmail.com>
2025-02-14 05:34:56 +0530

    Made changes to Book borrowing and Returning

--- src/Faculty.java
+++ src/Faculty.java
@@ -11,41 +11,50 @@ public class Faculty extends User {
 	}
 	public void borrow(String book, LocalDate l, Library lib) {
 		Book b = lib.get_book(book);
-		if(this.acc.books.size() >= 5) {
-			System.out.println("Can't borrrow book (5 books laready borrowed)");
-		}
-		else if(this.acc.books.size() == 0) {
-			if(b.avail == 1) {
-				this.acc.books.add(b);
-				this.acc.borrow_date.add(l);
-				b.avail = 0;
+		if(b != null) {
+			if(this.acc.books.size() >= 5) {
+				System.out.println("Can't borrrow book (5 books laready borrowed)");
+			}
+			else if(this.acc.books.size() == 0) {
+				if(b.avail == 1) {
+					this.acc.books.add(b);
+					this.acc.borrow_date.add(l);
+					b.avail = 0;
+				}
+				else {
+					System.out.println("Book not Available");
+				}
+			}
+			else if((int)ChronoUnit.DAYS.between(l, Collections.min(this.acc.borrow_date)) < -30) {
+				System.out.println("Can't borrrow book (Return book(s) borrowed 30 days or more back)");
 			}
 			else {
-				System.out.println("Book not Available");
+				if(b.avail == 1) {
+					this.acc.books.add(b);
+					this.acc.borrow_date.add(LocalDate.now());
+					b.avail = 0;
+					System.out.println("Book "+b.title+" Taken by "+this.name+"!");
+				}
+				else {
+					System.out.println("Book not Available");
+				}
 			}
-		}
-		else if((int)ChronoUnit.DAYS.between(l, Collections.min(this.acc.borrow_date)) < -30) {
-			System.out.println("Can't borrrow book (Return book(s) borrowed 30 days or more back)");
 		}
 		else {
-			if(b.avail == 1) {
-				this.acc.books.add(b);
-				this.acc.borrow_date.add(LocalDate.now());
-				b.avail = 0;
-				System.out.println("Book "+b.title+" Taken by "+this.name+"!");
-			}
-			else {
-				System.out.println("Book not Available");
-			}
+			System.out.println("Wrong Book Name!");
 		}
 	}
 	public void return_book(String book, Library lib) {
 		Book b = lib.get_book(book);
-		int x = this.acc.books.indexOf(b);
-		this.acc.books.remove(b);
-		this.acc.borrow_date.remove(x);
-		b.avail = 1;
-		
+		if(b!=null) {
+			int x = this.acc.books.indexOf(b);
+			this.acc.books.remove(b);
+			this.acc.borrow_date.remove(x);
+			b.avail = 1;
+		}
+		else {
+			System.out.println("Wrong Book Name!");
+		}
 	}
 	public void no_of_books() {
 		System.out.println(this.acc.books.size());
--- src/Student.java
+++ src/Student.java
@@ -19,14 +19,19 @@ public class Student extends User {
 		}
 		else {
 			Book book = lib.get_book(b);
-			if(book.avail == 1) {
-				this.acc.books.add(book);
-				this.acc.borrow_date.add(l);
-				book.avail = 0;
-				System.out.println("Book "+book.title+" Taken by "+this.name+"!");
+			if(book != null) {
+				if(book.avail == 1) {
+					this.acc.books.add(book);
+					this.acc.borrow_date.add(l);
+					book.avail = 0;
+					System.out.println("Book "+book.title+" Taken by "+this.name+"!");
+				}
+				else {
+					System.out.println("Book not Available");
+				}
 			}
 			else {
-				System.out.println("Book not Available");
+				System.out.println("Wrong Book Name!");
 			}
 		}
 	}
@@ -35,13 +40,18 @@ public class Student extends User {
 	}
 	public void return_book(String book, LocalDate l, Library lib) {
 		Book b = lib.get_book(book);
-		int x = this.acc.books.indexOf(b);
-		if((int)ChronoUnit.DAYS.between(l, this.acc.borrow_date.get(x)) < -15) {
-			this.acc.fine = this.acc.fine - 10 * ((int)ChronoUnit.DAYS.between(l, this.acc.borrow_date.get(x))) - 150;
+		if(b!=null) {
+			int x = this.acc.books.indexOf(b);
+			if((int)ChronoUnit.DAYS.between(l, this.acc.borrow_date.get(x)) < -15) {
+				this.acc.fine = this.acc.fine - 10 * ((int)ChronoUnit.DAYS.between(l, this.acc.borrow_date.get(x))) - 150;
+			}
+			this.acc.books.remove(b);
+			this.acc.borrow_date.remove(x);
+			System.out.println("Returned "+b.title+" successfully!");
 		}
-		this.acc.books.remove(b);
-		this.acc.borrow_date.remove(x);
-		System.out.println("Returned "+b.title+" successfully!");
+		else {
+			System.out.println("Wrong Book Name!");
+		}
 	}
 	public void check_fine(LocalDate l) {
 		int x = this.acc.check_fine(l);
